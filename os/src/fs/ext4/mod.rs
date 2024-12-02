a2d3fcfd8e274e2fe59bd9c50ba4389e479ef805
T202410701994223 <1021538027@qq.com>
2024-12-02 23:29:01 +0800

    修改ext4内容

--- os/src/fs/ext4/mod.rs
+++ os/src/fs/ext4/mod.rs
@@ -8,5 +8,5 @@ pub use crate::drivers::block::BlockDevice;
 use bitmap::Ext4;
 pub use efs::EasyFileSystem;
 // pub use layout::DiskInodeType;
-pub use crate::fs::vfs::Inode;
+pub use crate::fs::fat32::vfs::Inode;
 pub use ext4_rs::fuse_interface::*;
