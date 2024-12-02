b2fce23431260b6799ca7992996cab55340d92c3
T202410701994223 <1021538027@qq.com>
2024-12-03 04:48:27 +0800

    作适当修改以对应模块内文件的修改

--- os/src/fs/mod.rs
+++ os/src/fs/mod.rs
@@ -1,8 +1,8 @@
 mod cache;
 mod dev;
 pub mod directory_tree;
-mod fat32;
 mod ext4;
+mod fat32;
 pub mod file_trait;
 mod filesystem;
 mod layout;
@@ -10,25 +10,26 @@ pub mod poll;
 #[cfg(feature = "swap")]
 pub mod swap;
 // Xein add this
-pub mod inode;
-pub mod file_descriptor;
-mod vfs;
 mod dirent;
+mod file_descriptor;
+mod inode;
+mod vfs;
 
-pub use file_descriptor::{FileDescriptor, FdTable};
-pub use self::dev::{hwclock::*, null::*, pipe::*, socket::*, tty::*, zero::*};
+pub use self::dev::{hwclock::*, pipe::*, socket::*};
+pub use file_descriptor::{FdTable, FileDescriptor};
+use filesystem::FS_Type;
 
 pub use self::dirent::*;
+pub use self::fat32::DiskInodeType;
 pub use self::layout::*;
 pub use crate::drivers::block::BlockDevice;
-pub use self::fat32::DiskInodeType;
 // TODO: ext4 support
 use self::cache::PageCache;
-use alloc::{
-    string::String,
-    sync::Arc,
-};
+use alloc::{string::String, sync::Arc};
 use lazy_static::*;
+
+// 当前的文件系统类型
+pub const CURR_FS_TYPE: FS_Type = FS_Type::Fat32;
 
 lazy_static! {
     pub static ref ROOT_FD: Arc<FileDescriptor> = Arc::new(FileDescriptor::new(
