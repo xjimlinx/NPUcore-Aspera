e6a537bace8c62a08cfa0c00cb34f5761cbe0490
T202410701994223 <1021538027@qq.com>
2024-12-17 10:19:02 +0800

    更新注释

--- os/src/drivers/block/block_dev.rs
+++ os/src/drivers/block/block_dev.rs
@@ -6,20 +6,21 @@ use crate::arch::BLOCK_SZ;
 /// e.g. Does read_block clean the rest part of the block to be zero for buf.len()!=BLOCK_SZ in write_block() & read_block()
 /// e.g. What if buf.len()<BLOCK_SZ for write_block?
 pub trait BlockDevice: Send + Sync + Any {
-    /// Read block from BlockDevice
-    /// # Argument
+    /// 从块设备读取块
+    /// # 参数
     /// * `block_id`: the first sector(block) number to be read
-    /// * `buf`: the buffer to store the coming data
+    /// * `block_id`: 要读取的第一个块号
+    /// * `buf`: 存储读取的数据
     /// # Panic
-    /// The function panics when the size of `buf` is not a multiple of BLOCK_SZ
+    /// 当buf大小不是BLOCK_SZ的整数倍时会崩溃
     fn read_block(&self, block_id: usize, buf: &mut [u8]);
 
-    /// Write block into the file system.
-    /// # Argument
-    /// * `block_id`: the first sector(block) number to be written to
-    /// * `buf`: the buffer to store the coming data
+    /// 将块写入块设备
+    /// # 参数
+    /// * `block_id`: 要写入的第一个块号
+    /// * `buf`: 存储要写入的数据
     /// # Panic
-    /// The function panics when the size of `buf` is not a multiple of BLOCK_SZ
+    /// 当buf大小不是BLOCK_SZ的整数倍时会崩溃
     fn write_block(&self, block_id: usize, buf: &[u8]);
 
     /// # Note
