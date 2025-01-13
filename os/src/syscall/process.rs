65108596b9ce0daca3d56718d6053ddfc96f8aa3
T202410701994223 <1021538027@qq.com>
2025-01-13 18:24:10 +0800

    Update process.rs

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -256,11 +256,17 @@ pub fn sys_uname(buf: *mut u8) -> isize {
     );
     // A little stupid but still efficient.
     const FIELD_OFFSET: usize = 65;
-    buffer.write_at(FIELD_OFFSET * 0, b"Linux\0");
-    buffer.write_at(FIELD_OFFSET * 1, b"debian\0");
-    buffer.write_at(FIELD_OFFSET * 2, b"5.10.0-7-riscv64\0");
-    buffer.write_at(FIELD_OFFSET * 3, b"#1 SMP Debian 5.10.40-1 (2021-05-28)\0");
-    buffer.write_at(FIELD_OFFSET * 4, b"riscv64\0");
+    // buffer.write_at(FIELD_OFFSET * 0, b"Linux\0");
+    // buffer.write_at(FIELD_OFFSET * 1, b"debian\0");
+    // buffer.write_at(FIELD_OFFSET * 2, b"5.10.0-7-riscv64\0");
+    // buffer.write_at(FIELD_OFFSET * 3, b"#1 SMP Debian 5.10.40-1 (2021-05-28)\0");
+    // buffer.write_at(FIELD_OFFSET * 4, b"riscv64\0");
+    // buffer.write_at(FIELD_OFFSET * 5, b"\0");
+    buffer.write_at(FIELD_OFFSET * 0, b"NPUcore\0");
+    buffer.write_at(FIELD_OFFSET * 1, b"xeinnious\0");
+    buffer.write_at(FIELD_OFFSET * 2, b"0.10.0-1-loongarch64\0");
+    buffer.write_at(FIELD_OFFSET * 3, b"#1 SMP Xein-Revo 0.10.0-1 (2025-01-10)\0");
+    buffer.write_at(FIELD_OFFSET * 4, b"loongarch64\0");
     buffer.write_at(FIELD_OFFSET * 5, b"\0");
     SUCCESS
 }
