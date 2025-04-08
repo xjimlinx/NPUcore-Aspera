4dfbdcea6d9fdb9fe794717c2cfeca04c021d04e
xjimlinx <1021538027@qq.com>
2025-04-08 11:45:41 +0800

    更新一些宏变量，可能有问题，需要注意

--- os/src/hal/arch/riscv/config.rs
+++ os/src/hal/arch/riscv/config.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 
+pub const MEMORY_SIZE: usize = 0x1000_0000;
 pub const TASK_SIZE: usize = 0xc000_0000;
 pub const ELF_DYN_BASE: usize = TASK_SIZE / 3 * 2;
 pub const USER_STACK_BASE: usize = TASK_SIZE - PAGE_SIZE;
@@ -8,16 +9,18 @@ pub const USER_HEAP_SIZE: usize = PAGE_SIZE * 20;
 
 pub const KERNEL_STACK_SIZE: usize = PAGE_SIZE * 2;
 #[cfg(not(feature = "board_fu740"))]
-pub const KERNEL_HEAP_SIZE: usize = PAGE_SIZE * 0x240;
+// pub const KERNEL_HEAP_SIZE: usize = PAGE_SIZE * 0x240;
+pub const KERNEL_HEAP_SIZE: usize = PAGE_SIZE * 0x3000;
 #[cfg(feature = "board_fu740")]
 pub const KERNEL_HEAP_SIZE: usize = PAGE_SIZE * 0x2000;
 pub const MMAP_BASE: usize = 0x6000_0000;
 pub const MMAP_END: usize = 0x8000_0000;
 
 // manually make usable memory space equal
-pub const MEMORY_START: usize = 0x8000_0000;
+pub const MEMORY_START: usize = 0x0000_0000_8000_0000;
 #[cfg(all(not(feature = "board_k210"), not(feature = "board_fu740")))]
-pub const MEMORY_END: usize = 0x809e_0000;
+// pub const MEMORY_END: usize = 0x809e_0000;
+pub const MEMORY_END: usize = MEMORY_START + MEMORY_SIZE;
 #[cfg(feature = "board_k210")]
 pub const MEMORY_END: usize = 0x8080_0000;
 #[cfg(feature = "board_fu740")]
@@ -29,9 +32,20 @@ pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
 pub const SIGNAL_TRAMPOLINE: usize = TRAMPOLINE - PAGE_SIZE;
 pub const TRAP_CONTEXT_BASE: usize = SIGNAL_TRAMPOLINE - PAGE_SIZE;
 
-pub const DISK_IMAGE_BASE: usize = 0x9000_0000;
+pub const MEMORY_PHYS: usize = 0x800_0000;
+pub const DISK_IMAGE_BASE: usize = 0x9000_0000 + MEMORY_PHYS;
+// pub const DISK_IMAGE_BASE: usize = MEMORY_END;
 
 pub const SYSTEM_TASK_LIMIT: usize = 128;
 pub const SYSTEM_FD_LIMIT: usize = 256;
 
-pub use crate::board::{CLOCK_FREQ, MMIO};
+pub const BLOCK_SZ: usize = 2048;
+
+pub use crate::hal::arch::rv_board::{CLOCK_FREQ, MMIO};
+
+#[macro_export]
+macro_rules! newline {
+    () => {
+        "\r\n"
+    };
+}
